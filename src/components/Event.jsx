5bc9f6bc04e9e8e919505252d5fc7119078dac56
Stephen Kitchen <stephen.kitchen-CIC-UK@uk.ibm.com>
2018-10-13 22:50:15 +0100

    Updated event to overlay time list

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -7,22 +7,62 @@ import './Event.css';
 
 
 export default class Event extends React.PureComponent {
+  static getHeight(start, end) {
+    return `${end - start}px`;
+  }
+
+  static getLeft(column) {
+    return '80px';
+  }
+
+  static getTime(start) {
+    const hours = 9 + Math.floor(start / 60);
+    const hoursText = (`00${hours}`).slice(-2);
+    const mins = start % 60;
+    const minsText = (`00${mins}`).slice(-2);
+
+    return `${hoursText}:${minsText}`;
+  }
+
+  static getTop(start) {
+    const offset = 9;
+    return `${offset + start}px`;
+  }
+
+  static getWidth(columnCount) {
+    return `calc(85vw/${columnCount})`;
+  }
+
   render() {
-    const { end, start } = this.props;
+    const {
+      column, totalColumns, end, start,
+    } = this.props;
+
+    const style = {
+      height: Event.getHeight(start, end),
+      left: Event.getLeft(column),
+      top: Event.getTop(start),
+      width: Event.getWidth(totalColumns),
+    };
+
     return (
-      <ListItem button id="event">
-        <ListItemText primary={`${start} -> ${end}`} />
+      <ListItem id="event" style={style}>
+        <ListItemText primary={Event.getTime(start)} />
       </ListItem>
     );
   }
 }
 
 Event.defaultProps = {
+  column: 1,
   end: 30,
   start: 0,
+  totalColumns: 1,
 };
 
 Event.propTypes = {
+  column: PropTypes.number,
   end: PropTypes.number,
   start: PropTypes.number,
+  totalColumns: PropTypes.number,
 };
--- src/components/Event.test.jsx
+++ src/components/Event.test.jsx
@@ -9,3 +9,21 @@ describe('Event component', () => {
     expect(component.exists()).toEqual(true);
   });
 });
+
+describe('getTime', () => {
+  it('should return 09:00 for input 0', () => {
+    expect(Event.getTime(0)).toEqual('09:00');
+  });
+
+  it('should return 09:30 for input 30', () => {
+    expect(Event.getTime(30)).toEqual('09:30');
+  });
+
+  it('should return 10:35 for input 95', () => {
+    expect(Event.getTime(95)).toEqual('10:35');
+  });
+
+  it('should return 15:00 for input 360', () => {
+    expect(Event.getTime(360)).toEqual('15:00');
+  });
+});
