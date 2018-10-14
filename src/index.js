3d857b00b52c4814b24245cc5df5afb605705244
Stephen Kitchen <stephen.kitchen-CIC-UK@uk.ibm.com>
2018-10-14 14:22:29 +0100

    Added response message to renderDay function

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import App from './components/App';
 
 import './index.css';
 
+
 /**
  * Render the app with the supplied events.
  * @param {array} events The events to display.
+ * @returns {string}
  */
 window.renderDay = function renderDay(events) {
   ReactDOM.render(<App events={events} />, document.getElementById('root'));
+  return 'Calendar updated';
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
